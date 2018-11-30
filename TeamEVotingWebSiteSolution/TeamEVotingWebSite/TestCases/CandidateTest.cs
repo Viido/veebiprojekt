14a4a651d1ae31caf8f2a2d1172eaf5d8fea4be5
Viido Kaur Lutsar <37119942+Viido@users.noreply.github.com>
2018-11-30 14:19:00 +0200

    Implement initial tests

---
+++ TeamEVotingWebSiteSolution/TeamEVotingWebSite/TestCases/CandidateTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace TeamEVotingWebSite.TestCases
+{
+    public class CandidateTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void CreateCandidateTest()
+        {
+            driver.Url = "https://teamevotingwebsite.azurewebsites.net/";
+            driver.FindElement(By.XPath("//a[@href='/Candidates/Index']")).Click();
+            driver.FindElement(By.XPath("//a[@href='/Candidates/Create']")).Click();
+
+            IWebElement firstName = driver.FindElement(By.Id("Candidate_FirstName"));
+            firstName.SendKeys("TestFirstName");
+
+            IWebElement lastName = driver.FindElement(By.Id("Candidate_LastName"));
+            lastName.SendKeys("TestLastName");
+
+            IWebElement age = driver.FindElement(By.Id("Candidate_Age"));
+            age.Clear();
+            age.SendKeys("30");
+
+            SelectElement region = new SelectElement(driver.FindElement(By.Id("Region_Id")));
+            region.SelectByIndex(1);
+
+            SelectElement faction = new SelectElement(driver.FindElement(By.Id("Faction_Id")));
+            faction.SelectByIndex(1);
+
+            driver.FindElement(By.CssSelector(".btn.btn-default")).Click();
+
+            //TODO delete the test candidate
+            //Thread.Sleep(5000);
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ TeamEVotingWebSiteSolution/TeamEVotingWebSite/TestCases/ChromeTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TeamEVotingWebSite
+{
+    public class ChromeTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void OpenChromeTest()
+        {
+            driver.Url = "https://teamevotingwebsite.azurewebsites.net/";
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            driver.Close();
+        }
+    }
+}
